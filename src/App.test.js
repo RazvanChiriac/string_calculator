6bba17e8f7079564ad1c96dfbf412979bc375fa3
Razvan <chiriac2@coventry.ac.uk>
2021-02-24 22:05:28 +0000

    V2 - built tests to check if Add() can handle unknown amount of numbers

--- src/App.test.js
+++ src/App.test.js
@@ -10,8 +10,12 @@ describe("Add", () => {
     expect(Add("1")).toBe(1);
     expect(Add("")).toBe(0);
     expect(Add("1,2")).toBe(3);
-    expect(Add("1,2,3")).toBe(
-      "Error: The function can handle only up to two numbers"
-    );
+  });
+
+  it("should handle an unknown amount of numbers", () => {
+    expect(Add("1,2,3")).toBe(6);
+    expect(Add("1,2,3,4")).toBe(10);
+    expect(Add("10,20,30,40,50,60,70")).toBe(280);
+    expect(Add("10,20,30,40,50,60,70,80")).toBe(360);
   });
 });
