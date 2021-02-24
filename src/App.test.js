59a6706244b1ca83d9f7cde23ec9c7070e0993df
Razvan <chiriac2@coventry.ac.uk>
2021-02-24 21:52:24 +0000

    V1 - built tests to check if Add() returns the sum of max 2 numbers

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,13 @@ describe("Add", () => {
     expect(Add("2")).toBe(2);
     expect(Add("40")).toBe(40);
   });
+
+  it("should handle up to two numbers,separated by commas, and return their sum", () => {
+    expect(Add("1")).toBe(1);
+    expect(Add("")).toBe(0);
+    expect(Add("1,2")).toBe(3);
+    expect(Add("1,2,3")).toBe(
+      "Error: The function can handle only up to two numbers"
+    );
+  });
 });
